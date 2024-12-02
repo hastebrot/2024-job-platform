e56edf5c5ec6e5bd4f7300bdddf4bd929f75337e
Benjamin Gudehus <hastebrot@gmail.com>
2024-12-02 11:00:47 +0100

    [#11] feat: Page layout component. (#15)

--- sources/platform-app/vite.config.ts
+++ sources/platform-app/vite.config.ts
@@ -4,4 +4,21 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: false,
+    minify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          "react-router": ["react-router"],
+        },
+      },
+    },
+  },
+  esbuild: {
+    keepNames: true,
+    minifyIdentifiers: true,
+    treeShaking: true,
+  },
 });
