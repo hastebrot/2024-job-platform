8634b1cb7bb628456be5a3e1e68e3c05571ba081
Benjamin Gudehus <hastebrot@gmail.com>
2024-12-02 18:49:06 +0100

    feat: Employee details header.

--- sources/platform-app/src/pages/IndexPage.tsx
+++ sources/platform-app/src/pages/IndexPage.tsx
@@ -1,22 +1,53 @@
+import { icons } from "../helpers/lucide";
 import { PageLayout } from "./PageLayout";
 
 export const IndexPage = () => {
   return (
     <PageLayout>
-      <div>
-        <header>
-          <div>
-            <span>Firstname</span>
-            <span>Lastname</span>
-            <span>Active</span>
+      <div className="px-[35px] py-[25px]">
+        <header className="flex items-center gap-[25px]">
+          <div className="flex items-center gap-[14px]">
+            <div className="shrink-0 flex items-center justify-center size-[36px] rounded-full border border-[#DADCE0] bg-[#FFFFFF]">
+              <icons.ArrowLeft className="text-[#5A5F69]" size={18} />
+            </div>
+            <div className="shrink-0 ml-[2px] flex items-center justify-center size-[52px] rounded-full bg-[#FFFFFF] overflow-clip">
+              <div className="bg-[#92D4CC] w-full h-full"></div>
+            </div>
+            <div className="flex flex-col items-start gap-y-[8px] leading-none">
+              <span className="font-semibold text-[16px] text-nowrap">Firstname Lastname</span>
+              <div className="h-[24px] px-[10px] gap-[4px] rounded-[24px] flex items-center bg-[#9DC082] text-[#FFFFFF]">
+                <span className="size-[6px] bg-[#FFFFFF] rounded-full"></span>
+                <span className="text-[12px]">Active</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Last Clocked In</span>
-            <span>A few seconds ago</span>
-            <span>Last Messaged</span>
-            <span>2 days ago</span>
-            <span>Employee ID</span>
-            <span>#EMP7</span>
+          <div className="h-[32px] w-[1px] bg-[#DADCE0]"></div>
+          <div className="grid grid-rows-[auto_auto] grid-flow-col gap-x-[28px] gap-y-[10px] text-[14px] leading-none">
+            <span className="text-nowrap text-[#4F555F]">Last Clocked In</span>
+            <span className="text-nowrap font-semibold">A few seconds ago</span>
+            <span className="text-nowrap text-[#4F555F]">Last Messaged</span>
+            <span className="text-nowrap font-semibold">2 days ago</span>
+            <span className="text-nowrap text-[#4F555F]">Employee ID</span>
+            <span className="text-nowrap font-semibold">#EMP7</span>
+          </div>
+          <div className="ml-auto flex items-center gap-[14px]">
+            <div className="flex items-center gap-[7px]">
+              <div className="shrink-0 flex items-center justify-center size-[36px] rounded-full border border-[#DADCE0] bg-[#FFFFFF]">
+                <icons.ChevronLeft className="text-[#5A5F69]" size={18} />
+              </div>
+              <div className="shrink-0 flex items-center justify-center size-[36px] rounded-full border border-[#DADCE0] bg-[#FFFFFF]">
+                <icons.ChevronRight className="text-[#5A5F69]" size={18} />
+              </div>
+            </div>
+            <span className="text-nowrap text-[14px] text-[#4F555F]">1 of 32</span>
+            <div className="shrink-0 flex items-center justify-center size-[36px] rounded-[8px] border border-[#DADCE0] bg-[#FFFFFF]">
+              <icons.MoreVertical className="text-[#5A5F69]" size={18} />
+            </div>
+            <div className="h-[20px] w-[1px] bg-[#DADCE0]"></div>
+            <div className="shrink-0 flex items-center justify-center h-[36px] px-[14px] gap-[7px] rounded-[8px] bg-[#356966]">
+              <icons.Mail className="text-[#FFFFFF]" size={18} />
+              <span className="text-[#FFFFFF] text-nowrap text-[14px]">Send Email</span>
+            </div>
           </div>
         </header>
         <nav>
