a3c10ba4b21adb5a9423dac539f1a51a2fcd9a3e
Benjamin Gudehus <hastebrot@gmail.com>
2024-12-05 10:45:43 +0100

    [#14] feat: Employee list badges. (#25)

--- sources/platform-app/src/pages/EmployeeListPage.tsx
+++ sources/platform-app/src/pages/EmployeeListPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { classNames } from "../helpers/clsx";
 import { randomEmployee } from "../helpers/faker";
 import { icons } from "../helpers/lucide";
 import { range } from "../helpers/utils";
@@ -32,6 +33,21 @@ export const EmployeeListPage = () => {
             </div>
           </div>
         </header>
+
+        <section className="mt-[10px] flex items-center gap-[7px]">
+          <div className="h-[24px] px-[10px] gap-[4px] rounded-[24px] inline-flex items-center">
+            <span className="shrink-0 size-[6px] bg-[#9DC082] rounded-full"></span>
+            <span className="text-nowrap text-[12px] text-[#1D212C]">Active 28</span>
+          </div>
+          <div className="h-[24px] px-[10px] gap-[4px] rounded-[24px] inline-flex items-center">
+            <span className="shrink-0 size-[6px] bg-[#1D212C] rounded-full"></span>
+            <span className="text-nowrap text-[12px] text-[#1D212C]">Invited 1</span>
+          </div>
+          <div className="h-[24px] px-[10px] gap-[4px] rounded-[24px] inline-flex items-center">
+            <span className="shrink-0 size-[6px] bg-[#989EAD] rounded-full"></span>
+            <span className="text-nowrap text-[12px] text-[#989EAD]">Inactive 4</span>
+          </div>
+        </section>
 
         <section className="mt-[32px] flex items-center justify-between gap-[7px]">
           <div className="flex items-center gap-[7px]">
@@ -110,7 +126,7 @@ export const EmployeeListPage = () => {
                 <TableHeaderCell isSortable icon={icons.ContactRound}>
                   <span>Contact</span>
                 </TableHeaderCell>
-                <TableHeaderCell isSortable icon={icons.Calendar}>
+                <TableHeaderCell isFullWidth isSortable icon={icons.Calendar}>
                   <span>Joined</span>
                 </TableHeaderCell>
                 <TableHeaderCell></TableHeaderCell>
@@ -135,10 +151,22 @@ export const EmployeeListPage = () => {
                     <span className="text-nowrap truncate">Human Resources</span>
                   </TableCell>
                   <TableCell>
-                    <div className="h-[24px] px-[10px] gap-[4px] rounded-[24px] inline-flex items-center bg-[#9DC082] text-[#FFFFFF]">
-                      <span className="shrink-0 size-[6px] bg-[#FFFFFF] rounded-full"></span>
-                      <span className="text-nowrap text-[12px]">Active</span>
-                    </div>
+                    {
+                      [
+                        <div className="h-[24px] px-[10px] gap-[4px] rounded-[24px] inline-flex items-center bg-[#9DC082] text-[#FFFFFF]">
+                          <span className="shrink-0 size-[6px] bg-[#FFFFFF] rounded-full"></span>
+                          <span className="text-nowrap text-[12px]">Active</span>
+                        </div>,
+                        <div className="h-[24px] px-[10px] gap-[4px] rounded-[24px] inline-flex items-center bg-[#1D212C] text-[#FFFFFF]">
+                          <span className="shrink-0 size-[6px] bg-[#FFFFFF] rounded-full"></span>
+                          <span className="text-nowrap text-[12px]">Invited</span>
+                        </div>,
+                        <div className="h-[24px] px-[10px] gap-[4px] rounded-[24px] inline-flex items-center bg-[#B7BBC6] text-[#FFFFFF]">
+                          <span className="shrink-0 size-[6px] bg-[#FFFFFF] rounded-full"></span>
+                          <span className="text-nowrap text-[12px]">Inactive</span>
+                        </div>,
+                      ][Math.floor(3 * Math.random())]
+                    }
                   </TableCell>
                   <TableCell>
                     <span className="text-nowrap border border-[#DFE1E5] text-[#1777BD] rounded-full px-[8px] h-[18px]">
@@ -176,13 +204,20 @@ const Table = (props: TableProps) => {
 type TableHeaderCellProps = {
   children?: React.ReactNode;
   icon?: icons.LucideIcon;
+  isFullWidth?: boolean;
   isSortable?: boolean;
   isAddable?: boolean;
 };
 
 const TableHeaderCell = (props: TableHeaderCellProps) => {
   return (
-    <th className="px-[16px] text-[13px] font-normal text-left border border-t-0 first:border-l-0 last:border-r-0 border-[#DFE1E5]">
+    <th
+      className={classNames(
+        "px-[16px] text-[13px] font-normal text-left",
+        "border border-t-0 first:border-l-0 last:border-r-0 border-[#DFE1E5]",
+        props.isFullWidth && "w-full"
+      )}
+    >
       <div className="flex items-center gap-[4px]">
         {props.icon && <props.icon className="text-[#1D212C]" size={14} />}
         <span>{props.children}</span>
