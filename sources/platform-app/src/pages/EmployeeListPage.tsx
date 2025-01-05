41d5c7cb224e0ed5406ad472e8bdddb34dc056b7
Benjamin Gudehus <hastebrot@gmail.com>
2025-01-05 17:01:50 +0100

    [#34] feat: Employee details page, contract tab. (#51)

--- sources/platform-app/src/pages/EmployeeListPage.tsx
+++ sources/platform-app/src/pages/EmployeeListPage.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import { Employee, randomEmployee } from "../helpers/faker";
 import { icons } from "../helpers/lucide";
 import { range } from "../helpers/utils";
+import { PageLayout } from "../layouts/PageLayout";
 import { EmployeeListCardView } from "../pagelets/EmployeeListCardView";
 import { EmployeeListTableView } from "../pagelets/EmployeeListTableView";
-import { PageLayout } from "./PageLayout";
+
+type ViewOptions = "table" | "card";
 
 export const EmployeeListPage = () => {
   const headerLeft = (
@@ -38,6 +40,7 @@ export const EmployeeListPage = () => {
       </div>
     </div>
   );
+  const selectedView = "card" as ViewOptions;
   const [employeeList, setEmployeeList] = useState<Employee[]>([]);
   useEffect(() => {
     const employeeList = range(0, 10).map(() => {
@@ -140,10 +143,8 @@ export const EmployeeListPage = () => {
         </section>
 
         <section className="mt-[20px]">
-          <div className="hidden">
-            <EmployeeListTableView employeeList={employeeList} />
-          </div>
-          <EmployeeListCardView employeeList={employeeList} />
+          {selectedView === "table" && <EmployeeListTableView employeeList={employeeList} />}
+          {selectedView === "card" && <EmployeeListCardView employeeList={employeeList} />}
         </section>
       </div>
     </PageLayout>
--- sources/platform-app/src/pages/PeoplePage.tsx
+++
@@ -1,5 +0,0 @@
-import { PageLayout } from "./PageLayout";
-
-export const PeoplePage = () => {
-  return <PageLayout>people page</PageLayout>;
-};
